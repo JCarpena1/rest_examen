eaa0347094b638c22f5b48ba18d75e8397fce6cc
Antonio José García Jiménez <aj@losgarciajimenez.com>
2023-01-21 17:36:43 +0100

    Llamadas CRUD

--- src/main/java/es/eldelbit/rest/resources/ClientesResource.java
+++ src/main/java/es/eldelbit/rest/resources/ClientesResource.java
@@ -1,20 +1,45 @@
 package es.eldelbit.rest.resources;
 
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.Response;
 
 /**
  *
- * @author 
+ * @author
  */
 @Path("clientes")
 public class ClientesResource {
-    
+
     @GET
-    public Response ping(){
-        return Response
-                .ok("ping Jakarta EE")
-                .build();
+    public Response index() {
+        return Response.ok("index").build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response show(@PathParam("id") int id) {
+        return Response.ok("show " + id).build();
+    }
+
+    @POST
+    public Response store() {
+        return Response.ok("post").build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response update(@PathParam("id") int id) {
+        return Response.ok("update " + id).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response destroy(@PathParam("id") int id) {
+        return Response.ok("destroy " + id).build();
     }
 }
